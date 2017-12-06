a654e9c9cda21289febf40d10e8f9c835ac28f37
hcaula <henrique.caula@hotmail.com>
2017-12-06 07:37:20 -0300

    Add gameover

--- src/Main.hs
+++ src/Main.hs
@@ -22,15 +22,21 @@ main = do
 gameCycle :: IOGame GameAttribute () () () ()
 gameCycle = do
   (Elements (p1Score, p2Score, hits)) <- getGameAttribute
-  printOnScreen (show p1Score) TimesRoman24 (0,0) 1.0 1.0 1.0
-  printOnScreen (show p2Score) TimesRoman24 (50,0) 1.0 1.0 1.0
-  printOnScreen (show hits) TimesRoman24 (100,0) 1.0 1.0 1.0
 
   ball <- findObject "ball" "ballGroup"
   p1 <- findObject "P1" "playerGroup"
   p2 <- findObject "P2" "playerGroup"
   p1ScoreDisplay <- findObject "ScoreP1" "scoreGroup"
   p2ScoreDisplay <- findObject "ScoreP2" "scoreGroup"
+
+  when (p1Score == 9 || p2Score == 9) (do
+                                        setObjectSpeed (0,0) ball
+                                        setGameAttribute (Elements (11,11,0)))
+
+
+  printOnScreen (show p1Score) TimesRoman24 (0,0) 1.0 1.0 1.0
+  printOnScreen (show p2Score) TimesRoman24 (50,0) 1.0 1.0 1.0
+  printOnScreen (show hits) TimesRoman24 (100,0) 1.0 1.0 1.0
 
   let (vx,vy) = getGameObjectSpeed ball
   printOnScreen (show vx) TimesRoman24 (150,0) 1.0 1.0 1.0
@@ -52,10 +58,10 @@ gameCycle = do
   col6 <- objectRightMapCollision ball
   when (col5) (do
               setGameAttribute (Elements (p1Score, p2Score + 1, hits))
-              setObjectCurrentPicture (p2Score+2) p2ScoreDisplay)
+              (setObjectCurrentPicture (p2Score+2) p2ScoreDisplay))
   when (col6) (do
               setGameAttribute (Elements (p1Score + 1, p2Score, hits))
-              setObjectCurrentPicture (p1Score+2) p1ScoreDisplay)
+              (setObjectCurrentPicture (p1Score+2) p1ScoreDisplay))
   when (col5 || col6) (do
                       (setObjectPosition middleScreen ball)
                       (Elements (points1, points2, hits)) <- getGameAttribute
