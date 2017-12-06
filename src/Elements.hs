b48342a9fc81739076dfab4ee46ab51bb5edc2bd
hcaula <henrique.caula@hotmail.com>
2017-12-06 03:29:50 -0300

    Change bars and ball sprites

--- src/Elements.hs
+++ src/Elements.hs
@@ -1,14 +1,22 @@
 module Elements where
 import Settings
 import Graphics.UI.Fungen
+import Graphics.Rendering.OpenGL (GLdouble)
 
 createBall :: GameObject ()
 createBall =
-  let ballPic = Basic (Circle 6.0 0.0 1.0 0.0 Filled)
+  let ballBound = ball
+      ballPic = Basic (Polyg ballBound 1.0 1.0 1.0 Filled)
   in object "ball" ballPic False (w/2,h/2) (-8,8) ()
 
 createPlayer :: String -> GameObject ()
 createPlayer player =
-  let barBound = [(-25,-6),(25,-6),(25,6),(-25,6)]
+  let barBound = bar
       barPic   = Basic (Polyg barBound 1.0 1.0 1.0 Filled)
-  in object player barPic False (w/2,30) (0,0) ()
+  in object player barPic False (initialPosition player) (0,0) ()
+
+initialPosition :: String -> (GLdouble, GLdouble)
+initialPosition player =
+  if player == "P1" then (offset, h/2)
+  else if player == "P2" then (w-offset, h/2);
+  else (0,0)
--- src/Settings.hs
+++ src/Settings.hs
@@ -1,8 +1,30 @@
 module Settings where
 import Graphics.Rendering.OpenGL (GLdouble)
 
-width = 400 :: Int
-height = 400 :: Int
+-- Screen resolution in pixels
+width = 800 :: Int
+height = 600 :: Int
 
+-- Screen resolution converted to double
 w = fromIntegral width :: GLdouble
 h = fromIntegral height :: GLdouble
+
+-- Bars sprites measures
+bar :: [(Double, Double)]
+bar = [(-barWidth, -barHeight), (barWidth, -barHeight),(barWidth, barHeight),(-barWidth, barHeight)]
+
+-- Ball sprite measures
+ball :: [(Double, Double)]
+ball = [(-ballSize, -ballSize), (ballSize, -ballSize), (ballSize, ballSize), (-ballSize, ballSize)]
+
+-- Width of the bar: 2.5% of screen width
+barWidth = w/50
+
+-- Height of the bar: 12.5% of screen height
+barHeight = h/8
+
+-- Offset (distance between bar and screen horizontal border)
+offset = barWidth*2
+
+-- Ball size
+ballSize = barWidth;
