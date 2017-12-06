e092fc74cc1e62e8209d71df69af9bb56b044d45
hcaula <henrique.caula@hotmail.com>
2017-12-06 05:40:26 -0300

    Add comments and some balance changes

--- src/Inputs.hs
+++ src/Inputs.hs
@@ -16,11 +16,21 @@ movement = [
 
 moveP1Up :: Modifiers -> Position -> IOGame GameAttribute () () () ()
 moveP1Up _ _ = do
-  obj     <- findObject "P1" "playerGroup"
+
+  -- Gets the player object
+  obj <- findObject "P1" "playerGroup"
+
+  -- Sets its position to the pX and pY attrs
   (pX,pY) <- getObjectPosition obj
+
+  -- Sets its Y size to the sY size
   (_,sY)  <- getObjectSize obj
+
+  -- If the object won't pass the screen after adding the movement
   if (pY + (sY/2) + barSpeed <= h)
+    -- Sum its y position to the barSpeed (established in Globals.hs)
    then (setObjectPosition (pX,(pY + barSpeed)) obj)
+   -- Else, lock it to the maximum height position
    else (setObjectPosition (pX,(h - (sY/2))) obj)
 
 moveP1Down :: Modifiers -> Position -> IOGame GameAttribute () () () ()
@@ -34,7 +44,7 @@ moveP1Down _ _ = do
 
 moveP2Up :: Modifiers -> Position -> IOGame GameAttribute () () () ()
 moveP2Up _ _ = do
-  obj     <- findObject "P2" "playerGroup"
+  obj <- findObject "P2" "playerGroup"
   (pX,pY) <- getObjectPosition obj
   (_,sY)  <- getObjectSize obj
   if (pY + (sY/2) + barSpeed <= h)
--- src/Objects.hs
+++ src/Objects.hs
@@ -5,16 +5,19 @@ import Graphics.Rendering.OpenGL (GLdouble)
 
 createBall :: GameObject ()
 createBall =
+  -- The ballBound is set on Globals.hs
   let ballBound = ball
       ballPic = Basic (Polyg ballBound 1.0 1.0 1.0 Filled)
-  in object "ball" ballPic False (w/2,h/2) (-8,8) ()
+  in object "ball" ballPic False (w/2,h/2) (ballInitialSpeed,ballInitialSpeed) ()
 
 createPlayer :: String -> GameObject ()
 createPlayer player =
+  -- The barBound is set on Globals.hs
   let barBound = bar
       barPic   = Basic (Polyg barBound 1.0 1.0 1.0 Filled)
   in object player barPic False (initialPosition player) (0,0) ()
 
+-- Auxiliar function to get each player initial position
 initialPosition :: String -> (GLdouble, GLdouble)
 initialPosition player =
   if player == "P1" then (offset, h/2)
